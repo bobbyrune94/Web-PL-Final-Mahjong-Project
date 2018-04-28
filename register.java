96fa2a7105566cdf33ea66723af59e77a0f9e882
connor <cms3my@virginia.edu>
2018-04-28 17:10:42 -0400

    persist

---
+++ register.java
@@ -0,0 +1,143 @@
+import java.io.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+// libraries for parsing and manipulating XML file
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+@WebServlet("/register")
+public class register extends HttpServlet 
+{
+  private Document create_DOM_from_file(String fname) throws Exception 
+	   {
+	      try {
+	         File datafile = new File(fname);
+	         DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+	         DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
+	         Document doc = dbuilder.parse(datafile);
+	         return doc;
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+	      return null;
+  } 
+  
+  public void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException 
+  {
+	response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    String username = request.getParameter("user");
+    
+    if (username != null) {
+    	try {
+            Document doc = create_DOM_from_file("/../WebContent/WebINF/data/login.xml");
+            Element newUser = new Element("user");
+            newUser.addContent(new Element("name").setText(username));
+            newUser.addContent(new Element("pass").setText(request.getParameter("pass")))
+            document.getRootElement().addContent(works);   
+            XMLOutputter xmlOutput = new XMLOutputter();    
+            xmlOutput.output(document, System.out);
+            xmlOutput.setFormat(Format.getPrettyFormat());
+            xmlOutput.output(document,new FileWriter("/../WebContent/WebINF/data/login.xml")));
+         } catch (Exception e) {
+        e.printStackTrace();
+         }
+    }
+    else {
+	  String this_html =
+          "<!DOCTYPE html>" +
+          "<html lang='en'>" +
+          "<head>" +
+          "    <meta name='viewport' content='width=device-width, initial-scale=1'>" +  
+	      "        <link rel='stylesheet' type='text/css' href='styles/mainStyles.css' />" +
+	      "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' />" +
+          "    <title>Mahjong | Register</title>" +
+          "    <style>" +
+          "        label {" +
+      	  "            display: block;" +
+          "        }" +
+          "    </style>" +  
+          "</head>" +  
+          "<body>" +
+          "    <div class='container' style='text-align: center'>" +
+          "    <br/>" +
+          "    <div class='row'>" +
+          "        <!-- the below column was added so that the form would be centered until the window became too small -->" +
+          "        <div class='col-md-5'>" +
+          "        </div>" +
+          "        <!-- the below column holds the registration form -->" +
+          "        <div class='col-md-3'>" +
+          "        <form action='http://localhost:8080/WebPLProject/register' method='post' style='text-align: left' class='container'> " +
+          "           <label>Username: </label> <input type='text' name='user' autofocus required />" +
+          "           <!-- user-msg is used to inform users of invalid username entries -->" +
+          "           <div style='color:red' id='user-msg'></div>" +
+          "           <br/>" +
+          "           <label>Password: </label> <input type='password' id ='pass' name='pass' required />" +
+          "          <br/>" +
+          "          <input type='checkbox' id='showPass'> Show password" +
+          "          <br/>" +
+          "          <input class='btn btn-primary btn-lg' type='submit' value='Log in'/>" +
+          "      </form>" +
+          "      <a class=\"brn btn-primary btn-lg\" href=\"register.java\" role=\"button\"> No Account? Click Here to Register </a>"
+          "  </div>" +
+          "</div>" +
+          "</div>" +
+          "<script>" +
+          "// checks to see if the box is checked for Show password and either shows or hides the password text field depending on if the box was checked or not, respectively" +
+          "(function() {" +
+          "//fetch elements for password and show password fields" +
+          "  var password = document.getElementById('pass');" +
+          "  var show = document.getElementById('showPass');" +
+          "  //monitor the show password field" +
+          "  show.addEventListener('change', function(){" +
+          "  	//show or hide password text based on the show password field" +
+          "        if (show.checked)" +
+          "	        password.type='text';" +
+          "        else" +
+          "	        password.type='password';" +
+          " }, false);" +
+          "}());" +
+          "// checks the given login for length and displays a warning when a given username is too short" +
+          "// also checks the given password against 'password', directing to the main page only if the two strings are equal" +
+          "function validateLogin(){" +
+          "//fetch the given username and password" +
+          " var username = document.getElementByName('user').value;" +
+          " var password = document.getElementById('pass').value;" +
+          " //check validity of username through length" +
+          " if (username.length <= 5){ " +
+          " 	//inform of invalid username if given username is too short" +
+          "        document.getElementById('user-msg').innerHTML = 'Username is too short';" +
+          "        document.getElementByName('user').focus();" +
+      	  "     return false; " +
+      	  " }" +
+      	  " return true;" +
+          "}" +
+          "</script>" +
+          "</body>" +
+          "</html>";
+          out.println(this_html);
+      
+      out.close();
+    }
+  }
+}
+
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+		   throws ServletException, IOException 
+{
+	   doPost(request, response);
+}
+}
--- servletLogin.java
+++ servletLogin.java
@@ -1,8 +1,6 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.text.SimpleDateFormat;  
-import java.util.Date;  
+import java.io.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,12 +9,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+// libraries for parsing and manipulating XML file
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 @WebServlet("/servletLogin")
 public class servletLogin extends HttpServlet 
 {
 
    String msg = "";
-   
+      // create a DOM object
+   private Document create_DOM_from_file(String fname) throws Exception 
+   {
+      try {
+         File datafile = new File(fname);
+         DocumentBuilderFactory dbfactory = DocumentBuilderFactory.newInstance();
+         DocumentBuilder dbuilder = dbfactory.newDocumentBuilder();
+         Document doc = dbuilder.parse(datafile);
+         return doc;
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      return null;
+   } 
    public void doPost(HttpServletRequest request, HttpServletResponse response) 
                throws ServletException, IOException 
    {
@@ -26,10 +45,43 @@ public class servletLogin extends HttpServlet
       HttpSession session = request.getSession();
       String username = request.getParameter("user");      
       
+      boolean userExists = False;
+      boolean correctPass = False;
+      String pass = null;
       if (username != null)
-      {    	 	 
-    	  session.setAttribute("username", username);
-    	  
+      {    
+          
+          try {
+             Document doc = create_DOM_from_file("/../WebContent/WebINF/data/login.xml");
+
+             NodeList nList = doc.getElementsByTagName("user");
+             // iterate children of nList 
+             for (int i = 0; i < nList.getLength(); i++) 
+             {
+                Node nd = nList.item(i);
+            
+                // check if nd is an XML element, get values of its attributes and children
+                if (nd.getNodeType() == Node.ELEMENT_NODE) 
+                {
+                   Element ele = (Element)nd;
+               
+                   if (username.compare(ele.getElementsByTagName("name").item(0).getTextContent()) == 0) {
+                	   userExists = True;
+                	   if (request.getParameter("password") != null && request.getParameter("password").compare(ele.getElementsByTagName("pass").item(0).getTextContent()) == 0) {
+                		    correctPass = True;  
+                		    break;
+                	   }
+                   }
+                                  
+                }
+             }
+          } catch (Exception e) {
+         e.printStackTrace();
+          } 
+      }
+      if (userExists && correctPass) {
+	      session.setAttribute("username", username);
+	  
     	  //response.sendRedirect("http://localhost:80/CS4640Project/mainpage.php");
           out.println("<html>");
           out.println("<head>");
@@ -41,92 +93,105 @@ public class servletLogin extends HttpServlet
           out.println("    </form>");
           out.println("</body>");
           out.println("</html>");
+          out.close();
       }
-      else
-      {
-          String this_html =
-              "<!DOCTYPE html>" +
-              "<html lang='en'>" +
-              "<head>" +
-              "    <meta name='viewport' content='width=device-width, initial-scale=1'>" +  
+      else {
+      String this_html =
+          "<!DOCTYPE html>" +
+          "<html lang='en'>" +
+          "<head>" +
+          "    <meta name='viewport' content='width=device-width, initial-scale=1'>" +  
 	      "        <link rel='stylesheet' type='text/css' href='styles/mainStyles.css' />" +
-	      "        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' />" +
-              "    <title>Mahjong | Login</title>" +
-              "    <style>" +
-              "        label {" +
-      	      "            display: block;" +
-              "        }" +
-              "    </style>" +  
-              "</head>" +  
-              "<body>" +
-              "    <div class='container' style='text-align: center'>" +
-              "    <br/>" +
-              "    <div class='row'>" +
-              "        <!-- the below column was added so that the form would be centered until the window became too small -->" +
-              "        <div class='col-md-5'>" +
-              "        </div>" +
-              "        <!-- the below column holds the login form -->" +
-              "        <div class='col-md-3'>" +
-              "        <form action='http://localhost:8080/WebPLProject/servletLogin' method='post' style='text-align: left' class='container'> " +
-              "           <label>Username: </label> <input type='text' name='user' autofocus required />" +
-              "           <!-- user-msg is used to inform users of invalid username entries -->" +
-              "           <div style='color:red' id='user-msg'></div>" +
-              "           <br/>" +
-              "           <label>Password: </label> <input type='password' id='pass' required />" +
-              "          <br/>" +
-              "          <input type='checkbox' id='showPass'> Show password" +
-              "          <!-- pass-msg is used to inform users of an incorrect password entry -->" +
-              "          <div style='color:red' id='pass-msg'></div>" +
-              "          <br/>" +
-              "          <input class='btn btn-primary btn-lg' type='submit' value='Log in'/>" +
-              "      </form>" +
-              "  </div>" +
-              "</div>" +
-              "</div>" +
-              "<script>" +
-              "// checks to see if the box is checked for Show password and either shows or hides the password text field depending on if the box was checked or not, respectively" +
-              "(function() {" +
-              "//fetch elements for password and show password fields" +
-              "  var password = document.getElementById('pass');" +
-              "  var show = document.getElementById('showPass');" +
-              "  //monitor the show password field" +
-              "  show.addEventListener('change', function(){" +
-              "  	//show or hide password text based on the show password field" +
-              "        if (show.checked)" +
-              "	        password.type='text';" +
-              "        else" +
-              "	        password.type='password';" +
-              " }, false);" +
-              "}());" +
-              "// checks the given login for length and displays a warning when a given username is too short" +
-              "// also checks the given password against 'password', directing to the main page only if the two strings are equal" +
-              "function validateLogin(){" +
-              "//fetch the given username and password" +
-              " var username = document.getElementByName('user').value;" +
-              " var password = document.getElementById('pass').value;" +
-              " //check validity of username through length" +
-              " if (username.length <= 5){ " +
-              " 	//inform of invalid username if given username is too short" +
-              "        document.getElementById('user-msg').innerHTML = 'Username is too short';" +
-              "        document.getElementByName('user').focus();" +
-      	      "     return false; " +
-      	      " }" +
-      	      " //check password accuracy-- later we will have a database call here" +
-              " if (password === 'password'){" +
-              "        return true;" +
-              " }" +
-              " //inform of incorrect password if given password is not 'password' " +
-              " document.getElementById('pass-msg').innerHTML = 'Incorrect password';" +
-              " document.getElementById('pass').focus();" +
-              " return false;" +
-              "}" +
-              "</script>" +
-              "</body>" +
-              "</html>";
-              out.println(this_html);
-      }
+	      "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' />" +
+          "    <title>Mahjong | Login</title>" +
+          "    <style>" +
+          "        label {" +
+      	  "            display: block;" +
+          "        }" +
+          "    </style>" +  
+          "</head>" +  
+          "<body>" +
+          "    <div class='container' style='text-align: center'>" +
+          "    <br/>" +
+          "    <div class='row'>" +
+          "        <!-- the below column was added so that the form would be centered until the window became too small -->" +
+          "        <div class='col-md-5'>" +
+          "        </div>" +
+          "        <!-- the below column holds the login form -->" +
+          "        <div class='col-md-3'>" +
+          "        <form action='http://localhost:8080/WebPLProject/servletLogin' method='post' style='text-align: left' class='container'> " +
+          "           <label>Username: </label> <input type='text' name='user' autofocus required />" +
+          "           <!-- user-msg is used to inform users of invalid username entries -->" +
+          "           <div style='color:red' id='user-msg'>";
+          if (!userExists) {
+        	  thishtml +=
+        			  "User does not exist";
+          }
+      thishtml +=
+    	  "</div>" +
+          "           <br/>" +
+          "           <label>Password: </label> <input type='password' id ='pass' name='pass' required />" +
+          "          <br/>" +
+          "          <input type='checkbox' id='showPass'> Show password" +
+          "          <!-- pass-msg is used to inform users of an incorrect password entry -->" +
+          "          <div style='color:red' id='pass-msg'>";
+          if (userExists && !correctPass) {
+        	  thishtml +=
+        			  "Incorrect Password";
+          }
+      thishtml +=
+          "</div>" +
+          "          <br/>" +
+          "          <input class='btn btn-primary btn-lg' type='submit' value='Log in'/>" +
+          "      </form>" +
+          "      <a class=\"brn btn-primary btn-lg\" href=\"register.java\" role=\"button\"> No Account? Click Here to Register </a>"
+          "  </div>" +
+          "</div>" +
+          "</div>" +
+          "<script>" +
+          "// checks to see if the box is checked for Show password and either shows or hides the password text field depending on if the box was checked or not, respectively" +
+          "(function() {" +
+          "//fetch elements for password and show password fields" +
+          "  var password = document.getElementById('pass');" +
+          "  var show = document.getElementById('showPass');" +
+          "  //monitor the show password field" +
+          "  show.addEventListener('change', function(){" +
+          "  	//show or hide password text based on the show password field" +
+          "        if (show.checked)" +
+          "	        password.type='text';" +
+          "        else" +
+          "	        password.type='password';" +
+          " }, false);" +
+          "}());" +
+          "// checks the given login for length and displays a warning when a given username is too short" +
+          "// also checks the given password against 'password', directing to the main page only if the two strings are equal" +
+          "function validateLogin(){" +
+          "//fetch the given username and password" +
+          " var username = document.getElementByName('user').value;" +
+          " var password = document.getElementById('pass').value;" +
+          " //check validity of username through length" +
+          " if (username.length <= 5){ " +
+          " 	//inform of invalid username if given username is too short" +
+          "        document.getElementById('user-msg').innerHTML = 'Username is too short';" +
+          "        document.getElementByName('user').focus();" +
+      	  "     return false; " +
+      	  " }" +
+      	  " //check password accuracy-- later we will have a database call here" +
+          " if (password === 'password'){" +
+          "        return true;" +
+          " }" +
+          " //inform of incorrect password if given password is not 'password' " +
+          " document.getElementById('pass-msg').innerHTML = 'Incorrect password';" +
+          " document.getElementById('pass').focus();" +
+          " return false;" +
+          "}" +
+          "</script>" +
+          "</body>" +
+          "</html>";
+          out.println(this_html);
       
       out.close();
+      }
    }
    public void doGet(HttpServletRequest request, HttpServletResponse response)
 		   throws ServletException, IOException 
